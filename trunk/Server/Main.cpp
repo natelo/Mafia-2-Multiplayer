39a8c1cb689881da81af13cf875e4a791d17395b
nate <nate@8cfec0bd-d3a4-4003-b275-27d06a529d82>
2018-03-26 13:25:23 +0000

    - Fixed casting warning - Formated main.cpp ..
    git-svn-id: svn://svn.rocksteady-hosting.com/m2mp@20 8cfec0bd-d3a4-4003-b275-27d06a529d82

--- trunk/Server/Main.cpp
+++ trunk/Server/Main.cpp
@@ -21,115 +21,115 @@ std::thread				m_inputThread;
 std::queue<std::string>	m_inputQueue;
 unsigned long			m_ulStartTime;
 
-void					Shutdown( void );
+void					Shutdown(void);
 
 #ifdef WIN32
-BOOL WINAPI EventHandler( DWORD dwEvent )
+BOOL WINAPI EventHandler(DWORD dwEvent)
 {
-	switch( dwEvent )
+	switch (dwEvent)
 	{
 	case CTRL_CLOSE_EVENT: case CTRL_LOGOFF_EVENT: case CTRL_SHUTDOWN_EVENT:
-		{
-			pCore->SetActive(false);
-			break;
-		}
+	{
+		pCore->SetActive(false);
+		break;
+	}
 	}
 	return TRUE;
 }
 #else
-void SignalHandler( int iSignal )
+void SignalHandler(int iSignal)
 {
-    switch( iSignal )
-    {
-        case 1:
-        {
-			pCore->SetActive(false);
-            break;
-        };
+	switch (iSignal)
+	{
+	case 1:
+	{
+		pCore->SetActive(false);
+		break;
+	};
 
-        case SIGINT:
-        {
-        	CLogFile::Print( "Please use the 'quit' command instead of ctrl+c" );
-        	break;
-        };
-    }
+	case SIGINT:
+	{
+		CLogFile::Print("Please use the 'quit' command instead of ctrl+c");
+		break;
+	};
+	}
 }
 #endif
 
-void ConsoleInput( String strInput )
+void ConsoleInput(String strInput)
 {
-	if( strInput.IsEmpty() )
+	if (strInput.IsEmpty())
 		return;
 
-	size_t s = strInput.Find( " ", 0 );
-	String strCommand = strInput.substr( 0, s++ );
-	String strParams = strInput.substr( s, (strInput.GetLength() - s) );
+	size_t s = strInput.Find(" ", 0);
+	String strCommand = strInput.substr(0, s++);
+	String strParams = strInput.substr(s, (strInput.GetLength() - s));
 
-	if( s == 0 )
+	if (s == 0)
 	{
-		strParams.Truncate( 0 );
+		strParams.Truncate(0);
 	}
 
-	if( strCommand == "say" )
+	if (strCommand == "say")
 	{
-		if( strParams.IsEmpty() )
+		if (strParams.IsEmpty())
 		{
-			CLogFile::Print( "USAGE: say <message>" );
+			CLogFile::Print("USAGE: say <message>");
 			return;
 		}
 		RakNet::BitStream pBitStream;
-		pBitStream.Write( RakNet::RakString( "Server: %s", strParams.Get() ) );
-		pCore->GetNetworkModule()->Call( RPC_SENDPLAYERMESSAGE, &pBitStream, HIGH_PRIORITY, RELIABLE, INVALID_ENTITY_ID, true );
+		pBitStream.Write(RakNet::RakString("Server: %s", strParams.Get()));
+		pCore->GetNetworkModule()->Call(RPC_SENDPLAYERMESSAGE, &pBitStream, HIGH_PRIORITY, RELIABLE, INVALID_ENTITY_ID, true);
 
-		CLogFile::Printf( "[chat] Server: %s", strParams.Get() );
+		CLogFile::Printf("[chat] Server: %s", strParams.Get());
 	}
-	else if( strCommand == "exit" || strCommand == "quit" || strCommand == "shutdown" || strCommand == "terminate" )
+	else if (strCommand == "exit" || strCommand == "quit")
 	{
-		pCore->SetActive( false );
+		pCore->SetActive(false);
 	}
-	else if( strCommand == "start" )
+	else if (strCommand == "start")
 	{
-		if( strParams.IsEmpty() )
+		if (strParams.IsEmpty())
 		{
-			CLogFile::Print( "USAGE: start <resource>" );
+			CLogFile::Print("USAGE: start <resource>");
 			return;
 		}
-		if( pCore->GetResourceManager()->StartResource( strParams ) )
+		if (pCore->GetResourceManager()->StartResource(strParams))
 		{
-			CLogFile::Printf( "Successfully started resource '%s'.", strParams.Get() );
+			CLogFile::Printf("Successfully started resource '%s'.", strParams.Get());
 		}
 	}
-	else if( strCommand == "stop" )
+	else if (strCommand == "stop")
 	{
-		if( strParams.IsEmpty() )
+		if (strParams.IsEmpty())
 		{
-			CLogFile::Print( "USAGE: stop <resource>" );
+			CLogFile::Print("USAGE: stop <resource>");
 			return;
 		}
-		if( pCore->GetResourceManager()->StopResource( strParams ) )
+		if (pCore->GetResourceManager()->StopResource(strParams))
 		{
-			CLogFile::Printf( "Successfully stopped resource '%s'.", strParams.Get() );
+			CLogFile::Printf("Successfully stopped resource '%s'.", strParams.Get());
 		}
 	}
-	else if( strCommand == "restart" )
+	else if (strCommand == "restart")
 	{
-		if( strParams.IsEmpty() )
+		if (strParams.IsEmpty())
 		{
-			CLogFile::Print( "USAGE: restart <resource>" );
+			CLogFile::Print("USAGE: restart <resource>");
 			return;
 		}
-		if( pCore->GetResourceManager()->StopResource( strParams ) )
+		if (pCore->GetResourceManager()->StopResource(strParams))
 		{
-			pCore->GetResourceManager()->StartResource( strParams );
-			CLogFile::Printf( "Successfully restarted resource '%s'.", strParams.Get() );
+			pCore->GetResourceManager()->StartResource(strParams);
+			CLogFile::Printf("Successfully restarted resource '%s'.", strParams.Get());
 		}
 	}
-	else if( strCommand == "resources" )
+	else if (strCommand == "resources")
 	{
-		CLogFile::Print( "==============================================================" );
-		CLogFile::Print( "= List of running resources" );
+		CLogFile::Print("==============================================================");
+		CLogFile::Print("= List of running resources");
 		pCore->GetResourceManager()->GetRunningResources();
-		CLogFile::Print( "==============================================================" );
+		CLogFile::Print("==============================================================");
 	}
 	/*else if( strCommand == "netstats" || strCommand == "network" )
 	{
@@ -140,18 +140,18 @@ void ConsoleInput( String strInput )
 
 		CLogFile::Print( "==============================================================" );
 	}*/
-	else if( strCommand == "uptime" )
+	else if (strCommand == "uptime")
 	{
-		CLogFile::Print( "==============================================================" );
-		CLogFile::Printf( "= %s.", SharedUtility::GetTimePassedFromTime( m_ulStartTime ).Get() );
-		CLogFile::Print( "==============================================================" );
+		CLogFile::Print("==============================================================");
+		CLogFile::Printf("= %s.", SharedUtility::GetTimePassedFromTime(m_ulStartTime).Get());
+		CLogFile::Print("==============================================================");
 	}
 	else
 	{
 		CSquirrelArguments pArguments;
-		pArguments.push( strCommand );
-		pArguments.push( strParams );
-		pCore->GetEvents()->Call( "onConsoleInput", &pArguments );
+		pArguments.push(strCommand);
+		pArguments.push(strParams);
+		pCore->GetEvents()->Call("onConsoleInput", &pArguments);
 	}
 }
 
@@ -171,25 +171,25 @@ void InputThread()
 	}
 }
 
-int main( int argc, char * argv[] )
+int main(int argc, char * argv[])
 {
 	pCore = new CCore;
 
-	if( !pCore || !pCore->Initialise( argc, argv ) )
+	if (!pCore || !pCore->Initialise(argc, argv))
 	{
 #ifdef _WIN32
-		TerminateProcess( GetCurrentProcess(), 0 );
+		TerminateProcess(GetCurrentProcess(), 0);
 #else
 		_exit(42);
 #endif
 	}
 
 #ifdef _WIN32
-	SetConsoleCtrlHandler( (PHANDLER_ROUTINE)EventHandler, TRUE );
+	SetConsoleCtrlHandler((PHANDLER_ROUTINE)EventHandler, TRUE);
 #else
-	for( int i = 0; i < 15; i++ )
+	for (int i = 0; i < 15; i++)
 	{
-		signal( i, SignalHandler );
+		signal(i, SignalHandler);
 	}
 #endif
 
@@ -220,20 +220,20 @@ int main( int argc, char * argv[] )
 
 		Sleep(10);
 	}
-	Shutdown ();
+	Shutdown();
 	return 0;
 }
 
-void Shutdown( void )
+void Shutdown(void)
 {
 	m_readInput = false;
 	m_inputThread.join();
 
-	CLogFile::Printf( "Server shutting down..." );
+	CLogFile::Printf("Server shutting down...");
 
-	SAFE_DELETE( pCore );
+	SAFE_DELETE(pCore);
 #ifdef WIN32
-	TerminateProcess( GetCurrentProcess(), 0 );
+	TerminateProcess(GetCurrentProcess(), 0);
 #else
 	_exit(42);
 #endif
--- trunk/Shared/SharedUtility.cpp
+++ trunk/Shared/SharedUtility.cpp
@@ -121,7 +121,7 @@ namespace SharedUtility
 
 	unsigned long GetTime( void )
 	{
-		return (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+		return static_cast<unsigned int>((std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()));
 	}
 
 	bool Exists( const char * szPath )
